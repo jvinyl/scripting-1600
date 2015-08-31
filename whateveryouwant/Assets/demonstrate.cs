cfca7eb297aa81c091070e6d4435104d525c1762
unknown <10729659@CS514-03.ad.uvu.edu>
2015-08-31 09:23:02 -0600

    Variables and Functions

---
+++ whateveryouwant/Assets/demonstrate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demonstrate : MonoBehaviour {
+
+	int thisInt = 22;
+
+	// Use this for initialization
+	void Start () {
+	
+		thisInt = MultiplyByThree (thisInt);
+
+		Debug.Log(thisInt); 
+
+	}
+
+	int MultiplyByThree(int number) {
+		
+		int retValue;
+		retValue = number * 3;
+		return retValue;
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
