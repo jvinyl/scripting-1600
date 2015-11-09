ead1087a0a2bfd5781ee3dc6aaadcbb1c1e764af
unknown <10729659@CS514-03.ad.uvu.edu>
2015-11-09 09:30:19 -0700

    Arrays Assignment

---
+++ Arrays/Assets/Arrays.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour {
+
+	float[] myCollection = new float [3];
+	float[] myOtherCollection = {25, 50, 75, 100, 125};
+	public GameObject[] player = new GameObject [4];
+	public GameObject[] enemy;
+	// Use this for initialization
+	void Start () {
+	
+		player = GameObject.FindGameObjectsWithTag("Player");
+		myCollection [0] = 10;
+		myCollection [1] = 20;
+		myCollection [2] = 30;
+		myOtherCollection [4] = 99;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Arrays/Assets/ArraysSample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArraysSample : MonoBehaviour {
+
+	float[] myCollection = new float[4]; 
+	float[] myOtherCollection = {3, 4, 7, 99, 5};
+	public GameObject[] enemy;
+	public GameObject[] player = new GameObject[2];
+
+	// Use this for initialization
+	void Start () {
+
+		myCollection [0] = 45;
+		myCollection [1] = 100;
+		myCollection [2] = 33;
+		myCollection [3] = 80;
+		myOtherCollection [2] = 65;
+	
+
+		for (int i = 0; i < myCollection.Length; i++)
+			Debug.Log (myCollection[i]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
