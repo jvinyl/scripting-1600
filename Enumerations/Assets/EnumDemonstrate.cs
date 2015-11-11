f1412ed6127646980f3031eeaaf7a50142058f81
unknown <10729659@CS514-03.ad.uvu.edu>
2015-11-11 09:48:45 -0700

    Enumerations Assignment

---
+++ Enumerations/Assets/EnumDemonstrate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumDemonstrate : MonoBehaviour {
+
+	enum Weekdays {Sunday, Monday, Tuesday};
+	Weekdays myday1;
+	// Use this for initialization
+	void Start () {
+
+
+		myday1 = Weekdays.Sunday;
+
+		myday1 = nextDay (myday1);
+
+
+
+	}
+
+	Weekdays nextDay (Weekdays next)
+	{
+		if (next == Weekdays.Sunday)
+			Weekdays = Weekdays.Monday;
+		return next;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Enumerations/Assets/EnumSample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumSample : MonoBehaviour {
+
+	enum Weekdays{Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7};
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		Weekdays day1;
+		day1 = Weekdays.Sunday;
+	}
+
+	Weekdays TwoDaysAfter (Weekdays twoAft)
+	{
+		if (twoAft == Weekdays.Sunday)
+			twoAft = Weekdays.Tuesday;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
