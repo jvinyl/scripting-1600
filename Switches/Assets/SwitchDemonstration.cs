1637398c2c88e63f43027ad4fd7d0f6dfab5fec5
unknown <10729659@CS514-03.ad.uvu.edu>
2015-11-18 08:48:44 -0700

    Switch Assignment

---
+++ Switches/Assets/SwitchDemonstration.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchDemonstration : MonoBehaviour 
+{
+
+	public int satellites = 3;
+
+	void sky ()
+	{
+		switch (satellites) 
+		{
+		case 1:
+			print ("Sun");
+			break;
+
+		case 2:
+			print ("Moon");
+			break;
+
+		case 3:
+			print ("Star");
+			break;
+
+		default:
+			print ("None");
+			break;
+		}
+	}
+
+}
---
+++ Switches/Assets/SwitchExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchExample : MonoBehaviour {
+
+	public int response = 3;
+
+	void Answer ()
+	{
+		switch (response) 
+		{
+		case 3:
+			print ("Yes!");
+			break;
+		case 2:
+			print ("Maybe...");
+			break;
+		case 1: 
+			print ("...No...");
+			break;
+		default:
+			print ("...I don't know!");
+			break;
+		}
+	}
+}
